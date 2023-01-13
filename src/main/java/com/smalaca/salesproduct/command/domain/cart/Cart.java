14765a7a199e6682a6bd164c0bbe255a27e3b364
smalaca <ab13.krakow@gmail.com>
2023-01-13 14:06:43 +0100

    add product to cart - add customerId to Cart to specify its identity

--- src/main/java/com/smalaca/salesproduct/command/domain/cart/Cart.java
+++ src/main/java/com/smalaca/salesproduct/command/domain/cart/Cart.java
@@ -1,20 +1,18 @@
 package com.smalaca.salesproduct.command.domain.cart;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
-@Entity
 public class Cart {
-    @Id
-    @GeneratedValue
-    private UUID id;
-    private List<CartItem> cartItems = new ArrayList<>();
+    private final UUID customerId;
+    private final List<CartItem> cartItems = new ArrayList<>();
+
+    private Cart(UUID customerId) {
+        this.customerId = customerId;
+    }
 
     public void add(Map<String, Integer> items) {
         items.forEach((productCode, amount) -> {
